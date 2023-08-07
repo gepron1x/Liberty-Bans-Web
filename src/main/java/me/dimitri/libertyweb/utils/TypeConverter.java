028125048adf54c7175a57b98f90da6cd022ea6e
georgijpronuk@gmail.com <georgijpronuk@gmail.com>
2023-08-07 16:47:46 +0300

    improve TypeConverter

--- src/main/java/me/dimitri/libertyweb/utils/TypeConverter.java
+++ src/main/java/me/dimitri/libertyweb/utils/TypeConverter.java
@@ -4,20 +4,6 @@ import space.arim.libertybans.api.PunishmentType;
 
 public class TypeConverter {
     public static PunishmentType getType(String type) {
-        switch (type) {
-            case "ban" -> {
-                return PunishmentType.BAN;
-            }
-            case "mute" -> {
-                return PunishmentType.MUTE;
-            }
-            case "warn" -> {
-                return PunishmentType.WARN;
-            }
-            case "kick" -> {
-                return PunishmentType.KICK;
-            }
-        }
-        return PunishmentType.BAN;
+        return PunishmentType.valueOf(type.toUpperCase());
     }
 }
